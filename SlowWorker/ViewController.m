49806efa01c3307ece8ba479db159f7da5cc649f
Alex Chekodanov <checkodanov@gmail.com>
2018-08-30 13:56:17 +0300

    initial commit

--- SlowWorker/ViewController.m
+++ SlowWorker/ViewController.m
@@ -10,6 +10,10 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIButton *startButton;
+@property (weak, nonatomic) IBOutlet UITextView *resultTextView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
+
 @end
 
 @implementation ViewController
@@ -19,6 +23,52 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (NSString *)fetchSomethingFromServer
+{
+    [NSThread sleepForTimeInterval:1];
+    return @"Hi there";
+}
+
+- (NSString *)processData:(NSString *)data
+{
+    [NSThread sleepForTimeInterval:2];
+    return [data uppercaseString];
+}
+
+- (NSString *)calculateFirstResult:(NSString *)data
+{
+    [NSThread sleepForTimeInterval:3];
+    return [NSString stringWithFormat:@"Number of chars: %lu", [data length]];
+}
+
+- (NSString *)calculateSecondResult:(NSString *)data
+{
+    [NSThread sleepForTimeInterval:4];
+    return [data stringByReplacingOccurrencesOfString:@"E" withString:@"e"];
+}
+
+- (IBAction)doWork:(id)sender
+{
+    NSDate *startTime = [NSDate date];
+    self.startButton.enabled = NO;
+    
+    [self.spinner startAnimating];
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+        dispatch_async(queue, ^{
+        NSString *fetchedData = [self fetchSomethingFromServer];
+        NSString *processedData = [self processData:fetchedData];
+        NSString *firtstResult = [self calculateFirstResult:processedData];
+        NSString *secondResult = [self calculateSecondResult:processedData];
+        NSString *resultSummary = [NSString stringWithFormat:@"First: [%@]\nSecond: [%@]", firtstResult, secondResult];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.resultTextView.text = resultSummary;
+                self.startButton.enabled = YES;
+                [self.spinner stopAnimating];
+            });
+        NSDate *endTime = [NSDate date];
+        NSLog(@"Completed in %f seconds", [endTime timeIntervalSinceDate:startTime]);
+    });
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
